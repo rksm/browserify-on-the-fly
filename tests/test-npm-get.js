62cea4495dd8e715a476b45973831eef44d86647
Robert Krahn <robert.krahn@gmail.com>
2013-11-29 19:59:30 -0800

    initial commit, npm package loader exists

---
+++ tests/test-npm-get.js
@@ -0,0 +1,35 @@
+// continously run with:
+// nodemon nodeunit tests/websocket-test.js
+
+require('shelljs/global');
+var shelljs = require('shelljs');
+
+var testSuite = {},
+    npmLoader = require('../npm-loader'),
+    path = require('path'),
+    fs = require('fs');
+
+testSuite.NpmInstallPackageTest = {
+
+  setUp: function(run) {
+    run();
+  },
+
+  tearDown: function(run) {
+      npmLoader.cleanup(run);
+  },
+
+  "fetch package and put it into temp dir": function(test) {
+      npmLoader.install({name: 'async'}, function(err, result) {
+        test.ok(!err, err);
+        test.ok(fs.existsSync(result.location));
+        var asyncDir = path.join(result.location, 'node_modules', 'async');
+        test.ok(fs.existsSync(asyncDir));
+        var installedAsync = require(asyncDir);
+        test.ok(!!installedAsync.series, 'No async installed?');
+        test.done();
+      });
+  }
+}
+
+exports.testSuite = testSuite;
